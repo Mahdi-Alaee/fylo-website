539362a94f35370cabe931aa04f37fcc0b08aa87
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-01-25 19:39:05 +0330

    inital project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen">
+      <h1 className="text-center py-5 bg-gray-200 dark:bg-gray-800 dark:text-white text-4xl">
+        Project Is Stated
+      </h1>
+    </div>
+  );
+}
+
+export default App;
